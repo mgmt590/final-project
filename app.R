05530168a0f67f09e03e54fc58b708c9afd52c34
Nilofer Rajpurkar <nilofer@github.com>
2021-05-29 07:51:47 -0700

    Update app.R
    switch to k-means app

--- app.R
+++ app.R
@@ -1,25 +1,42 @@
-# lab - shiny_5
+# 01-kmeans-app
+
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput(inputId = "num", 
-    label = "Choose a number", 
-    value = 25, min = 1, max = 100),
-  plotOutput("hist"),
-  dataTableOutput("values")
+  headerPanel('Iris k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris)),
+    selectInput('ycol', 'Y Variable', names(iris),
+      selected = names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+      min = 1, max = 9)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
 )
 
 server <- function(input, output) {
-  output$hist <- renderPlot({
-    hist(rnorm(input$num))
+
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
   })
-  
-  output$values <- renderDataTable({
-    as.data.frame(rnorm(input$num))
-    
+
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
   })
+
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+
 }
 
 shinyApp(ui = ui, server = server)
-
